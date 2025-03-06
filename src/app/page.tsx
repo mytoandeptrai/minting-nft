d3ffd9d85168d65b1ba31e96d68118ce62a1ad49
Tran Toan <toantpm@var-meta.com>
2025-03-06 10:45:45 +0700

    feat: set up code base

---
+++ src/app/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { NftMint } from "@/components/nft-mint";
+import {
+   defaultChainId,
+   defaultNftContractAddress,
+   defaultTokenId,
+} from "@/lib/constants";
+import { client } from "@/lib/thirdwebClient";
+import { defineChain, getContract } from "thirdweb";
+import { getContractMetadata } from "thirdweb/extensions/common";
+import { getNFT, isERC1155 } from "thirdweb/extensions/erc1155";
+import { useReadContract } from "thirdweb/react";
+
+export default function Home() {
+   const tokenId = defaultTokenId;
+   const chain = defineChain(defaultChainId);
+   const contract = getContract({
+      address: defaultNftContractAddress,
+      chain,
+      client,
+   });
+   const isERC1155Query = useReadContract(isERC1155, { contract });
+   const contractMetadataQuery = useReadContract(getContractMetadata, {
+      contract,
+   });
+
+   const nftQuery = useReadContract(getNFT, {
+      contract,
+      tokenId,
+      queryOptions: { enabled: isERC1155Query.data },
+   });
+
+   const displayName = isERC1155Query.data
+      ? nftQuery.data?.metadata.name
+      : contractMetadataQuery.data?.name;
+
+   const description = isERC1155Query.data
+      ? nftQuery.data?.metadata.description
+      : contractMetadataQuery.data?.description;
+
+   return (
+      <NftMint
+         contract={contract}
+         displayName={displayName || ""}
+         symbol={contractMetadataQuery.data?.symbol || ""}
+         contractImage={contractMetadataQuery.data?.image || ""}
+         description={description || ""}
+         tokenId={tokenId}
+      />
+   );
+}
