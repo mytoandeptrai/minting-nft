8526a46fc00b864bba468ef5b621c7f987927909
Tran Toan <toantpm@var-meta.com>
2025-03-07 15:00:56 +0700

    feat: update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,8 @@ import { ToastProvider } from "@/components/ui/toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "NFT Minting template",
-	description: "A minting template powered by thirdweb",
+	title: "FORGE ESSENCE",
+	description: "FORGE ESSENCE",
 };
 
 export default function RootLayout({
