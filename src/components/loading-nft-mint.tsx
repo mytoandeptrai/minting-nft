c08ad48942e608167ba71d34e24c77a893bb9e6d
Tran Toan <toantpm@var-meta.com>
2025-03-10 14:16:03 +0700

    feat: update loading

--- src/components/loading-nft-mint.tsx
+++ src/components/loading-nft-mint.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Skeleton } from "@/components/ui/skeleton";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
@@ -7,43 +5,49 @@ import { Plus, Minus } from "lucide-react";
 
 export default function LoadingNftMint() {
    return (
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900">
-         <Card className="w-full max-w-md">
-            <CardContent className="pt-6">
-               <div className="aspect-square overflow-hidden rounded-lg mb-4 relative">
-                  <Skeleton className="w-full h-full" />
-               </div>
+      <div className="w-full min-h-screen bg-gray-900">
+         <div className="container mx-auto px-4 py-4 flex items-center justify-between bg-gray-900 mb-1">
+            <Skeleton className="h-12 w-80" />
+            <Skeleton className="h-12 w-40" />
+         </div>
+         <div className="w-full flex flex-col items-center justify-center flex-1 mt-28">
+            <Card className="w-full max-w-md">
+               <CardContent className="pt-6">
+                  <div className="aspect-square overflow-hidden rounded-lg mb-4 relative">
+                     <Skeleton className="w-full h-full" />
+                  </div>
 
-               <Skeleton className="h-8 w-3/4 mb-2" />
-               <Skeleton className="h-4 w-1/2 mb-4" />
+                  <Skeleton className="h-8 w-3/4 mb-2" />
+                  <Skeleton className="h-4 w-1/2 mb-4" />
 
-               <div className="flex items-center justify-center mb-4 w-full">
-                  <Button
-                     variant="outline"
-                     size="icon"
-                     className="rounded-r-none"
-                     disabled
-                  >
-                     <Minus className="h-4 w-4" />
-                  </Button>
-                  <Skeleton className="h-10 w-28 mx-2" />
-                  <Button
-                     variant="outline"
-                     size="icon"
-                     className="rounded-l-none"
-                     disabled
-                  >
-                     <Plus className="h-4 w-4" />
-                  </Button>
-               </div>
+                  <div className="flex items-center justify-center mb-4 w-full">
+                     <Button
+                        variant="outline"
+                        size="icon"
+                        className="rounded-r-none"
+                        disabled
+                     >
+                        <Minus className="h-4 w-4" />
+                     </Button>
+                     <Skeleton className="h-10 w-28 mx-2" />
+                     <Button
+                        variant="outline"
+                        size="icon"
+                        className="rounded-l-none"
+                        disabled
+                     >
+                        <Plus className="h-4 w-4" />
+                     </Button>
+                  </div>
 
-               <Skeleton className="h-4 w-3/4 mb-4" />
-            </CardContent>
+                  <Skeleton className="h-4 w-3/4 mb-4" />
+               </CardContent>
 
-            <CardFooter>
-               <Skeleton className="h-10 w-full" />
-            </CardFooter>
-         </Card>
+               <CardFooter>
+                  <Skeleton className="h-10 w-full" />
+               </CardFooter>
+            </Card>
+         </div>
       </div>
    );
 }
