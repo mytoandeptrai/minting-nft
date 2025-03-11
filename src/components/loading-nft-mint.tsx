64538ee7ee3a995841e19c2cec3dacc148daf19b
Tran Toan <toantpm@var-meta.com>
2025-03-11 15:04:16 +0700

    feat: update loading UI

--- src/components/loading-nft-mint.tsx
+++ src/components/loading-nft-mint.tsx
@@ -7,10 +7,10 @@ export default function LoadingNftMint() {
    return (
       <div className="w-full min-h-screen bg-dark-grey">
          <div className="container mx-auto px-4 py-4 flex items-center justify-between bg-dark-grey mb-1">
-            <Skeleton className="h-10 md:h-12 w-80" />
-            <Skeleton className="h-10 md:h-12 w-40" />
+            <Skeleton className="h-10 md:h-12 w-20 md:w-80" />
+            <Skeleton className="h-10 md:h-12 w-32 md:w-40" />
          </div>
-         <div className="w-full flex flex-col items-center justify-center flex-1 mt-28 pb-10">
+         <div className="w-full flex flex-col items-center justify-center px-4 md:px-0 md:flex-1 mt-4 md:mt-28 pb-10">
             <Card className="w-full max-w-md">
                <CardContent className="pt-6">
                   <div className="aspect-square overflow-hidden rounded-lg mb-4 relative">
@@ -47,6 +47,9 @@ export default function LoadingNftMint() {
                   <Skeleton className="h-10 md:h-12 w-full" />
                </CardFooter>
             </Card>
+         </div>
+         <div className="px-4 flex justify-end bg-dark-grey mb-1">
+            <Skeleton className="h-10 md:h-12 w-40" />
          </div>
       </div>
    );
