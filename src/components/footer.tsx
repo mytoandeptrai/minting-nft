5d4049216895eab97c2c97081e48b478e8d21701
Tran Toan <toantpm@var-meta.com>
2025-03-11 15:45:50 +0700

    fix: update link for icon

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,17 +1,19 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
    return (
       <div className="w-full flex justify-end p-4 dark:bg-dark-grey">
-         <div>
+         <Link className="block" href={process.env.NEXT_PUBLIC_AVALANCHE_REDIRECT!} target="_blank">
             <Image
                src="/avalanche-logo.svg"
                alt="avalanche-logo"
                width={150}
                height={50}
+               className="w-32"
             />
-         </div>
+         </Link>
       </div>
    );
 };
