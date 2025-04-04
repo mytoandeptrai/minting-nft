3e9b73461fe017d12bfdb10a84f44970789ab2fd
Nam Dang <namquoc.dev@gmail.com>
2025-04-04 09:12:51 +0700

    fix message

--- src/components/modules/nft-mint/nft-mint-header.tsx
+++ src/components/modules/nft-mint/nft-mint-header.tsx
@@ -45,9 +45,6 @@ const NftMintHeader = (props: Props) => {
 
    return (
       <>
-         <div className="flex flex-col items-center w-full">
-            <p className="text-center font-semibold text-lg text-red-500 my-4">FORGE ESSENCE Mint is now closed</p>
-         </div>
          <div className="aspect-square overflow-hidden rounded-lg">
             <NFT
                contract={props.contract}
@@ -61,6 +58,9 @@ const NftMintHeader = (props: Props) => {
             </NFT>
          </div>
          <div className="-mt-12">
+            <div className="flex flex-col items-center w-full">
+               <p className="text-center font-semibold text-lg text-red-500 my-4">FORGE ESSENCE Mint is now closed</p>
+            </div>
             <h2 className="text-2xl font-bold mb-2 dark:bg-gradient-to-b dark:from-[#FFB931] dark:to-[#FF7A00] inline-block dark:text-transparent dark:bg-clip-text">
                {props.displayName}
             </h2>
